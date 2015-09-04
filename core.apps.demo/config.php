25607f80c451ceff77154cff5dc7775684b08c2f
konstantin_popov <v8199@yandex.ru>
2015-09-04 12:42:20 +0600

    demo

---
+++ core.apps.demo/config.php
@@ -0,0 +1,31 @@
+<?php
+
+$config['js_apps']['core.apps.demo'] = array(
+
+    'general' => array(
+        'title' => 'Demo',
+        'name' => 'demo',//should be like 3th part of folder
+        'version' => '1.0.0',
+        'icon' => 'icon.png',
+        'category' => CATEGORY_TABS_SLIDES,
+        'description' => ''
+       
+    ),
+
+    'content' => array(
+        USERTYPE_ADMIN => array(
+            'code' => array(
+                'accordion.js',
+                'accordion.admin.js'
+            )
+        ),
+
+        USERTYPE_CONTRIBUTOR => array(
+            'code' => array('accordion.js')
+        ),
+
+        USERTYPE_GUEST => array(
+            'code' => array('accordion.js')
+        )
+    )
+);
